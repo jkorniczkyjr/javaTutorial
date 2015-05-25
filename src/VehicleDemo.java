e54b7c5a5b5f87618454f5824187ea4f306168a8
Joe <jkorniczkyjr@gmail.com>
2015-05-25 17:56:33 +0300

    updated code

--- src/VehicleDemo.java
+++ src/VehicleDemo.java
@@ -12,12 +12,19 @@ class Vehicle {
     int range() {
         return fuelcap * mpg;
     }
+
+    // Calculates gallons required for a given trip
+    double fuelNeeded(int miles) {
+        return (double) miles / mpg; // Need (double) because both are int
+    }
 }
 // This class declares an object of type Vehicle
 class VehicleDemo {
     public static void main(String[] args) {
         Vehicle minivan = new Vehicle();
         Vehicle sportscar = new Vehicle();
+        double gallons;
+        int dist = 252;
 
        // Assign values to fields in minivan
         minivan.passengers = 7;
@@ -30,7 +37,12 @@ class VehicleDemo {
         sportscar.mpg = 12;
 
         // Compute the range assuming a full tank of gas
-        System.out.print("Minivan can carry " + minivan.passengers + " and have a range of " + minivan.range() + " miles.\n");
-        System.out.print("Sportscar can carry " + sportscar.passengers + " and have a range of " + sportscar.range() + " miles.");
+        System.out.println("Minivan can carry " + minivan.passengers + " and have a range of " + minivan.range() + " miles.");
+        gallons = minivan.fuelNeeded(dist);
+        System.out.println("It will need " + gallons + " gallons to go " + dist + " miles.");
+        System.out.println();
+        System.out.println("Sportscar can carry " + sportscar.passengers + " and have a range of " + sportscar.range() + " miles.");
+        gallons = sportscar.fuelNeeded(dist);
+        System.out.println("It will need " + gallons + " gallons to go " + dist + " miles.");
     }
 }
